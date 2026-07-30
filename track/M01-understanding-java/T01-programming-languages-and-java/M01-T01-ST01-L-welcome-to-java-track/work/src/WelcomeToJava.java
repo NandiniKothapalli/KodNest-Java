c2dce7b25649163db55c94c38b597228c467696b
NandiniKothapalli <nandinikothapalli2@.com>
2026-07-30 12:47:57 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
+        System.out.println("Welcome to the Java Track");
+        System.out.println("I already know how to build programming Language");
+        System.out.println("Now I am learning how to express the logic using Java");
        //Write your code here
     }
 }
