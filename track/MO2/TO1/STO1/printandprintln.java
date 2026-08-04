cc3a66a5b5f9aa041aac39e1ca511a41c1ba6f26
NandiniKothapalli <nandinikothapalli2@.com>
2026-08-04 11:36:59 +0530

    printandprintln

---
+++ track/MO2/TO1/STO1/printandprintln.java
@@ -0,0 +1,14 @@
+
+public class printandprintln {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("wednesday");
+        System.out.println("Thursday");
+        System.out.print("Friday");
+        System.out.println("saturday");
+        System.out.println("sunday");
+
+    }
+}
